a3ded5f83a943611be9238b10102766bcefe59d1
Brent Tubbs <brent.tubbs@gmail.com>
2015-02-19 11:50:56 -0800

    Bump version: 0.0.2 → 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup_params = dict(
     name='mettle',
-    version='0.0.2',
+    version='0.1.0',
     author='Y Team',
     author_email=', '.join([
         'alejandro.rivera@yougov.com',
