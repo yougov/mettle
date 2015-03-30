d5986bbd282d2acf90f04af62d3022cd81bb70ab
Josh Harbaugh <josh.harbaugh@yougov.com>
2015-03-30 11:03:12 -0400

    Bump version: 0.2.10 → 0.2.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup_params = dict(
     name='mettle',
-    version='0.2.10',
+    version='0.2.11',
     author='Y Team',
     author_email=', '.join([
         'alejandro.rivera@yougov.com',
